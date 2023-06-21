5397b03631bcc10da3081ded54e5b3e109f28789
root <root@thierno>
2023-06-21 15:35:53 +0200

    push first

---
+++ src/main/java/com/example/forDemo/ForDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.forDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForDemoApplication.class, args);
+	}
+
+}
